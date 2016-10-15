33a497e01bfcbd9998f23321df8271b787252ba9
Nhi Quach <nhiquach@berkeley.edu>
2016-10-14 22:18:38 -0700

    pending step definition for admin step

--- features/step_definitions/oram_admin_steps.rb
+++ features/step_definitions/oram_admin_steps.rb
@@ -1,5 +1,6 @@
 Given /the following referrers exist/ do |referrers_rable|
-  referrers_table.hashes.each do |referrer|
-    Referrers.create!(referrer)
-  end
+  pending
+  # referrers_table.hashes.each do |referrer|
+  #   Referrers.create!(referrer)
+  # end
 end
