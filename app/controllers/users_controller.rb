0f26096d709b143c809904dee4c4a0b417b0fa04
Michael Chen <michael.chen@berkeley.edu>
2016-10-28 15:19:24 -0700

    added some initial approve/reject functions in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 	end
+
+	def show_referrers
+		@referrers = User.with_role :referrer
+	end
+
+	def mark_referrer_approved
+		id = [params[:id]]
+		@referrer = Users.find_by_id(id)
+		@referrer.approved = true
+		@referrer.save
+		redirect_to :show_referrers
+	end
+
+	def mark_referrer_rejected
+		id = [params[:id]]
+		@referrer = Users.find_by_id(id)
+		@referrer.approved = false
+		@referrer.save
+		redirect_to :show_referrers
+	end
 end
