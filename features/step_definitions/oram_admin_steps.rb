5caf73d74889405970bb8f1c9fb949f2f2b285ae
Nhi Quach <nhiquach@berkeley.edu>
2016-11-02 23:33:24 -0700

    testing for filtering and user invitations

--- features/step_definitions/oram_admin_steps.rb
+++ features/step_definitions/oram_admin_steps.rb
@@ -4,14 +4,6 @@ Given /the following admins exist/ do |admins_table|
   end
 end
 
-Given /I am an ORAM administrator/ do
-	pending
-end
-
-Then /I should see all case documents/ do |case_documents_table|
-  pending
-end
-
 Given /the following referrers exist/ do |referrers_table|
     referrers_table.hashes.each do |referrer|
       user = User.create(referrer)
--- features/step_definitions/referrer_vetting_steps.rb
+++ features/step_definitions/referrer_vetting_steps.rb
@@ -1,10 +1,10 @@
 And /I am logged in as the following admin/ do |admin_table|
     @admin = nil
-    
+
     admin_table.hashes.each do |admin|
         @admin = Admin.create(admin)
     end
-    
+
     visit new_admin_session_path
     fill_in("admin_email", :with => @admin.email)
     fill_in("admin_password", :with => @admin.password)
@@ -15,16 +15,16 @@ When /I view the profile of "(.*)\s(.*)"/ do |first_name, last_name|
     find(:xpath, "//tr[td[contains(.,'#{first_name}')]]/td/a", :text => 'View Profile').click
 end
 
-# When (/I select "([^"]*)" from the status dropdown/) do |option|
-#     if option == "Status"
-#         find("#select_button").find(:xpath, 'option[1]').select_option
-#     elsif option == "Incomplete"
-#         find("#select_button").find(:xpath, 'option[2]').select_option
-#     elsif option == "Complete"
-#         find("#select_button").find(:xpath, 'option[3]').select_option
-#     elsif option == "Approved"
-#         find("#select_button").find(:xpath, 'option[4]').select_option
-#     elsif option == "Rejected"
-#         find("#select_button").find(:xpath, 'option[5]').select_option
-#     end
-# end
+When (/I select "([^"]*)" from the status dropdown/) do |option|
+    if option == "Status"
+        find("option[value='Status']").click
+    elsif option == "Incomplete"
+        find("option[value='Incomplete']").click
+    elsif option == "Complete"
+      find("option[value='Complete']").click
+    elsif option == "Approved"
+      find("option[value='Approved']").click
+    elsif option == "Rejected"
+      find("option[value='Rejected']").click
+    end
+end
