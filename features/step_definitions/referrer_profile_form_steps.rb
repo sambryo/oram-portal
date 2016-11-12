5386ab8fb10408e879415ef2974fac9652becaf6
Michael Chen <michael.chen@berkeley.edu>
2016-11-11 18:41:55 -0800

    finished writing basic cucumber tests for referrer profile forms

---
+++ features/step_definitions/referrer_profile_form_steps.rb
@@ -0,0 +1,15 @@
+Given /I am logged in as the following referrer/ do |referrer_table|
+    # @referrer = nil
+    #
+    # referrer_table.hashes.each do |referrer|
+    #     @referrer = User.create(referrer)
+    # end
+    #
+    # @referrer.add_role :referrer
+    #
+    # visit new_user_session_path
+    # fill_in("user_email", :with => @admin.email)
+    # fill_in("User_password", :with => @admin.password)
+    # click_button("Log in")
+    pending
+end
