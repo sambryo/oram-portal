e1171349988e83c583273084fe162241650b041b
Nhi Quach <nhiquach@users.noreply.github.com>
2016-12-01 00:56:18 -0800

    nq/dropbox (#89)
    * dropbox file uploading
    
    * added route after upload
    
    * added doc link
    
    * update attr for client after save
    
    * change to url
    
    * multipart form
    
    * edited carrier wave config for dropbox access

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,5 +6,5 @@ CarrierWave.configure do |config|
   config.dropbox_access_token = ENV["dropbox_access_token"]
   config.dropbox_access_token_secret = ENV["dropbox_access_token_secret"]
   config.dropbox_user_id = ENV["dropbox_user_id"]
-  config.dropbox_access_type = "dropbox"
+  config.dropbox_access_type = "app_folder"
 end
