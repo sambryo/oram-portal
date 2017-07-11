a10f28f7c718d92add6193072feb000407164ce2
sambryo <terefesam@gmail.com>
2017-07-11 22:05:17 +0000

    Cucumber coverage is now 76.3%

--- features/step_definitions/admin_signup.rb
+++ features/step_definitions/admin_signup.rb
@@ -1,11 +1,11 @@
 # Signing up as an admin
 
-Given /I am on the sign up page/ do
+Given /^(?:|I ) am on the sign up page/ do
   pending
   #should be able to see an admin sign up possibilitiy
 end
 
-Given /I am on the admin sign page/ do
+Given /^(?:|I ) am on the admin sign page/ do
   pending
   #should be able to login in if I have credientials, otherwise can't
 end
