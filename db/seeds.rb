8f9c7db1ad9541007a309692c27f5db3ddbe2721
Shawn Shi <shawnshi101@berkeley.edu>
2017-07-12 23:11:58 +0000

    added test admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = Admin.new
+admin.first_name = "oram"
+admin.last_name = "admin"
+admin.email = "admin321@gmail.com"
+admin.password = "password"
+admin.save
