a1fe435086d5b172aabdb0eed4c9dba16048e792
Andrew Halle <ahalle@berkeley.edu>
2017-07-15 19:58:48 +0000

    fixed host in config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'oram-portal.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'oram-portal2.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
